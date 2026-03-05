a29d764463b6e471d6b90c0f9d728f100b49a6f2
Hilzyyy <164994772+xHilyZ@users.noreply.github.com>
2026-03-05 20:00:47 +1100

    Update getData.js

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -1,13 +1,14 @@
-export default async (req, context) => {
-  const db = context.env.ETERNALS_DB;
+export default async () => {
+    const kv = await import("@netlify/kv");
 
-  const cleanBalance = Number(await db.get("cleanBalance")) || 0;
-  const dirtyBalance = Number(await db.get("dirtyBalance")) || 0;
-  const transactions = JSON.parse(await db.get("transactions") || "[]");
+    const cleanBalance = Number(await kv.get("cleanBalance")) || 0;
+    const dirtyBalance = Number(await kv.get("dirtyBalance")) || 0;
 
-  return Response.json({
-    cleanBalance,
-    dirtyBalance,
-    transactions
-  });
+    const transactions = await kv.get("transactions") || [];
+
+    return Response.json({
+        cleanBalance,
+        dirtyBalance,
+        transactions
+    });
 };
