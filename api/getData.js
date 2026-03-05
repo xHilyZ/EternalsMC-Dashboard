5c7d236320651971c67a967fdddca04b4a319e53
Hilzyyy <164994772+xHilyZ@users.noreply.github.com>
2026-03-05 22:26:14 +1100

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -8,7 +8,9 @@ export default async function handler(req, res) {
     .select('*')
     .single();
 
-  if (error) return res.status(500).json({ error: error.message });
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
 
   res.status(200).json(data);
 }
