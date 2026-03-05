ae0a676dfa4b102b959e9c7c9517b5a9efb16834
Hilzyyy <164994772+xHilyZ@users.noreply.github.com>
2026-03-05 21:14:09 +1100

    Update updateFunds.js

--- netlify/functions/updateFunds.js
+++ netlify/functions/updateFunds.js
@@ -1,26 +1,22 @@
 import { getStore } from '@netlify/blobs';
 
 export const handler = async (event) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
-  const store = getStore('eternals-store');
-  let data = await store.get('eternals.json', { type: 'json' });
+    const store = getStore('eternals-store');
+    let data = await store.get('eternals.json', { type: 'json' });
 
-  if (!data) {
-    return { statusCode: 500, body: 'Data not initialized' };
-  }
+    const { cleanDelta = 0, dirtyDelta = 0 } = JSON.parse(event.body || '{}');
 
-  const { cleanDelta = 0, dirtyDelta = 0 } = JSON.parse(event.body || '{}');
+    data.funds.clean += cleanDelta;
+    data.funds.dirty += dirtyDelta;
 
-  data.funds.clean += cleanDelta;
-  data.funds.dirty += dirtyDelta;
+    await store.set('eternals.json', JSON.stringify(data));
 
-  await store.set('eternals.json', JSON.stringify(data));
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data.funds)
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data.funds)
+    };
 };
